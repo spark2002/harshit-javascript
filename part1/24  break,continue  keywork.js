9f6a259ea0be8c5dd5896a18b32a2fff110fce7b
spark2002 <102783351+spark2002@users.noreply.github.com>
2023-02-12 02:27:04 +0530

    Update 24  break,continue  keywork.js

--- part1/24  break,continue  keywork.js
+++ part1/24  break,continue  keywork.js
@@ -6,12 +6,12 @@
 //     if(i===4){
 //         break;
 //     }
-//     console.log(i);
+//     console.log(i);                  //1,2,3
 // }
 
 // for(let i = 1; i<=10; i++){
 //     if(i===4){
-//         continue;
+//         continue;                    //1,2,3,5,6,7,8,9,10
 //     }
 //     console.log(i);
 // }
