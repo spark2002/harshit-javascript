2d4da4081b386271f5abd1df0f7d2556a95bb58f
spark2002 <102783351+spark2002@users.noreply.github.com>
2023-02-12 02:24:14 +0530

    Update and rename 21.js to 21 while loop example .js

---
+++ part1/21 while loop example .js
@@ -0,0 +1,16 @@
+// while loop example 
+let num = 100;
+// let sum = 0; //1 + 2 +3
+// let i = 0;
+
+
+// while(i<=100){
+//     sum = sum + i;
+//     i++;
+// }
+
+
+// console.log(sum);
+
+// let sum = (num*(num+1))/2;     //shortcut
+// console.log(sum);
