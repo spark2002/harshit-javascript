47604a7f1e8ef8d24a5b5f7a29a361980e3b41ef
spark2002 <102783351+spark2002@users.noreply.github.com>
2023-02-11 17:38:45 +0530

    Update and rename 11.js to 11 undefined,null,bigint.js

---
+++ part1/11 undefined,null,bigint.js
@@ -0,0 +1,22 @@
+// undefined 
+// null
+
+// let firstName;
+// console.log(typeof firstName);                  // undefined
+// firstName = "Harshit";
+// console.log(typeof firstName, firstName);         // string Harshit 
+
+// let myVariable = null;
+// console.log(myVariable);                           //null
+// myVariable = "harshit";                           
+// console.log(myVariable, typeof myVariable);         harshit string
+// console.log(typeof null);                           object
+ bug , error 
+
+// BigInt
+ // let myNumber = BigInt(12);
+// let sameMyNumber = 123n;
+// // console.log(myNumber);                     //  12
+// // console.log(Number.MAX_SAFE_INTEGER);
+// console.log(myNumber+ sameMyNumber);            //  135
+                                                                                                                                         
