b565bffc696c832e53851d749c7bbc83309a0b1f
spark2002 <102783351+spark2002@users.noreply.github.com>
2023-02-14 09:44:53 +0530

    Update 31  use const for creating array.js

--- part1/31  use const for creating array.js
+++ part1/31  use const for creating array.js
@@ -3,5 +3,16 @@
  // heap memory ["apple", "mango"]      //0x11
 
  const fruits = ["apple", "mango"]; // 0x11
- fruits.push("banana");                            //(3) ['apple', 'mango', 'banana']
+ fruits.push("banana");                            //(3) ['apple', 'mango', 'banana']     
  console.log(fruits);
+ fruits.pop();
+ console.log(fruits);                               //(2) ['apple', 'mango']
+ fruits.unshift("grapes","pickle);
+ console.log(fruits);                               //(4) ['grapes', 'pickle', 'apple', 'mango']
+ fruits.shift();
+ console.log(fruits);                               //(3) ['pickle', 'apple', 'mango']
+
+//push--add
+//pop--remove
+//unshift--add
+//shift--remove
