c1112b9db92739a1024ef9e3e1defd269f7c6ef6
spark2002 <102783351+spark2002@users.noreply.github.com>
2023-02-14 08:51:49 +0530

    Update and rename 35.js to 35  array destructuring .js

---
+++ part1/35  array destructuring .js
@@ -0,0 +1,12 @@
+// array destructuring 
+const myArray = ["value1", "value2", "value3","value4"];
+// let myvar1 = myArray[0];
+// let myvar2 = myArray[1];
+// console.log("value of myvar1", myvar1);       //value1
+// console.log("value of myvar2", myvar2);       //value2
+
+let [myvar1, myvar2, ...myNewArray] = myArray;
+console.log("value of myvar1", myvar1);           //value1
+console.log("value of myvar2", myvar2);           //value2
+//myNewArray=MyArray.slice(2);
+console.log(myNewArray);                          //value3 value4
