2e4c8f136757598bc1299a882aa172cfea459f80
spark2002 <102783351+spark2002@users.noreply.github.com>
2023-02-14 09:29:34 +0530

    Update and rename 31.js to 31  use const for creating array.js

---
+++ part1/31  use const for creating array.js
@@ -0,0 +1,7 @@
+// use const for creating array
+
+ // heap memory ["apple", "mango"]      //0x11
+
+ const fruits = ["apple", "mango"]; // 0x11
+ fruits.push("banana");                            //(3) ['apple', 'mango', 'banana']
+ console.log(fruits);
