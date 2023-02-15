d2c6eb5df03dbc84ee3dbd7b8f7dcc8da0577c8c
spark2002 <102783351+spark2002@users.noreply.github.com>
2023-02-15 08:00:51 +0530

    Update 39.js

--- part1/39.js
+++ part1/39.js
@@ -6,19 +6,19 @@ const key2 = "objkey2";
 const value1 = "myvalue1";
 const value2 = "myvalue2";
 
-// const obj = {
-//     objkey1 : "myvalue1",
-//     objkey2 : "myvalue2",
-// }
-
-// const obj = {
-//     [key1] : value1,
-//     [key2] : value2
-// }
-
-const obj = {};
-
-obj[key1] = value1;
-obj[key2] = value2;
+const obj = {
+    key1: value1,
+    key2: value2
+}
 console.log(obj);
 
+const obj2 = {
+    [key1]: value1,
+    [key2]: value2
+}
+console.log(obj2);
+
+const obj3 = {}
+obj3[key1] = value1;
+obj3[key2] = value2;
+console.log(obj3);
